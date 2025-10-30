c6e06b64deb83baf363cf42b6f62ae88860740cc
Alex Lopes Nascimento <72759248+alloes@users.noreply.github.com>
2025-10-30 10:09:13 +0000

    add scripts `lint:check` e `lint:write`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um projeto para estudos com o objetivo de implementar o https://www.tabnews.com.br seguindo o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-function Home(){
-    return <h1> Já possuímos tecnologia suficiente para
-         produzir e distribuir tudo o que as pessoas do 
-         mundo precisam para viver de forma segura e confortável, 
-         e ainda reduzindo drasticamente a carga de trabalho. ⚒️ </h1>
+function Home() {
+  return (
+    <h1>
+      {" "}
+      Já possuímos tecnologia suficiente para produzir e distribuir tudo o que
+      as pessoas do mundo precisam para viver de forma segura e confortável, e
+      ainda reduzindo drasticamente a carga de trabalho. ⚒️{" "}
+    </h1>
+  );
 }
 
 export default Home;
